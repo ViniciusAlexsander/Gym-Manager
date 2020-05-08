ebd14137061c06c6485baf258dd4d2836cbdbd14
Vinicius Marinho <48867230+ViniciusAlexsander@users.noreply.github.com>
2020-05-08 13:24:30 -0300

    Terminei de fazer a relação entre instrutor e aluno, e um filtro de busca para os professores

--- src/app/controllers/members.js
+++ src/app/controllers/members.js
@@ -9,7 +9,9 @@ module.exports = {
     });
   },
   create(req, res) {
-    return res.render("members/create");
+    Member.instructorsSelectOptions(function (options) {
+      return res.render("members/create", { instructorOptions: options });
+    });
   },
   post(req, res) {
     const keys = Object.keys(req.body);
@@ -38,7 +40,12 @@ module.exports = {
 
       member.birth = date(member.birth).iso;
 
-      return res.render(`members/edit`, { member });
+      Member.instructorsSelectOptions(function (options) {
+        return res.render("members/edit", {
+          member,
+          instructorOptions: options,
+        });
+      });
     });
   },
   put(req, res) {
--- src/app/models/Member.js
+++ src/app/models/Member.js
@@ -19,8 +19,9 @@ module.exports = {
         birth,
         blood,
         weight,
-        height
-      ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+        height,
+        instructor_id
+      ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
       RETURNING id
     `;
 
@@ -33,6 +34,7 @@ module.exports = {
       dataFromBody.blood,
       dataFromBody.weight,
       dataFromBody.height,
+      dataFromBody.instructor,
     ];
 
     db.query(query, values, function (err, results) {
@@ -41,13 +43,18 @@ module.exports = {
     });
   },
   find(id, callback) {
-    db.query(`SELECT * FROM members WHERE id = $1`, [id], function (
-      err,
-      results
-    ) {
-      if (err) throw `Database Error! ${err}`;
-      callback(results.rows[0]);
-    });
+    db.query(
+      `
+      SELECT members.*, instructors.name AS instructor_name 
+      FROM members
+      LEFT JOIN instructors ON (members.instructor_id = instructors.id) 
+      WHERE members.id = $1`,
+      [id],
+      function (err, results) {
+        if (err) throw `Database Error! ${err}`;
+        callback(results.rows[0]);
+      }
+    );
   },
   update(data, callback) {
     const query = `
@@ -59,8 +66,9 @@ module.exports = {
         email=($5),
         blood=($6),
         weight=($7),
-        height=($8)
-      WHERE id = ($9)
+        height=($8),
+        instructor_id=($9)
+      WHERE id = ($10)
     `;
     const values = [
       data.avatar_url,
@@ -72,6 +80,7 @@ module.exports = {
       data.weight,
       data.height,
       data.id,
+      data.instructor,
     ];
 
     db.query(query, values, function (err, results) {
@@ -87,6 +96,13 @@ module.exports = {
       if (err) throw `Database Error! ${err}`;
 
       return callback();
+    });
+  },
+  instructorsSelectOptions(callback) {
+    db.query(`SELECT name,id FROM instructors`, function (err, results) {
+      if (err) throw `Database Error ${err}`;
+
+      callback(results.rows);
     });
   },
 };
